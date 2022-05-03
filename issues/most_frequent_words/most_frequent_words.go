a6d6befa1e7322709deb02da14bb2c009f3e1c6a
Anton Viktorov <anthony.viktorov@gmail.com>
2022-05-03 14:00:30 +0300

    codestyle fixes

--- issues/most_frequent_words/most_frequent_words.go
+++ issues/most_frequent_words/most_frequent_words.go
@@ -5,6 +5,8 @@ import (
 	"strings"
 )
 
+const resLen = 10
+
 func MostFrequentWords(text string) ([]string, error) {
 	words := strings.Fields(text)
 	repeats := make(map[string]int)
@@ -21,16 +23,12 @@ func MostFrequentWords(text string) ([]string, error) {
 	sort.Slice(keys, func(i, j int) bool {
 		if repeats[keys[i]] == repeats[keys[j]] {
 			return keys[i] < keys[j]
-		} else {
-			return repeats[keys[i]] > repeats[keys[j]]
 		}
+		return repeats[keys[i]] > repeats[keys[j]]
 	})
 
-	var resLen int
-	if len(keys) < 10 {
-		resLen = len(keys)
-	} else {
-		resLen = 10
+	if len(keys) < resLen {
+		return keys, nil
 	}
 
 	return keys[:resLen], nil
